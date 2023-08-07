2a5b42d716885c8a705d977cd3013af5417d6d89
Brandon Henness <Henness0666@gmail.com>
2023-08-07 09:16:54 -0700

    fixed google sign-in

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -1,238 +1,284 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:sign_in_button/sign_in_button.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  LoginScreenState createState() => LoginScreenState();
+}
+
+class LoginScreenState extends State<LoginScreen> {
   final _auth = FirebaseAuth.instance;
-
-  LoginScreen({Key? key}) : super(key: key);
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  String errorMessage = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(40),
-        child: Column(
-          children: [
-            const SizedBox(height: 50),
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'Sign in',
-                style: TextStyle(
-                  // fontWeight: FontWeight.bold,
-                  fontSize: 28,
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'Welcome to Pantry Planner! Please sign in to continue.',
-                style: TextStyle(
-                  // fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                ),
-              ),
-            ),
-            Row(
-              children: [
-                const Text(
-                  'Don\'t have an account?',
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(40),
+          child: Column(
+            children: [
+              const SizedBox(height: 50),
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Sign in',
                   style: TextStyle(
-                    color: Colors.grey, // Set the color here
+                    // fontWeight: FontWeight.bold,
+                    fontSize: 28,
                   ),
                 ),
-                TextButton(
-                  onPressed: () {
-                    // TODO: Implement registration functionality
-                  },
-                  child: const Text('Register'),
+              ),
+              const SizedBox(height: 20),
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Welcome to Pantry Planner! Please sign in to continue.',
+                  style: TextStyle(
+                    // fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
                 ),
-              ],
-            ),
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Enter your email',
               ),
-            ),
-            const SizedBox(height: 20),
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Password',
+              Row(
+                children: [
+                  const Text(
+                    'Don\'t have an account?',
+                    style: TextStyle(
+                      color: Colors.grey, // Set the color here
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      // TODO: Implement registration functionality
+                    },
+                    child: const Text('Register'),
+                  ),
+                ],
               ),
-              obscureText: true,
-            ),
-            const SizedBox(height: 20),
-            Align(
-              alignment: Alignment.centerRight,
-              child: TextButton(
-                onPressed: () {
-                  // TODO: Implement forgot password functionality
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  labelText: 'Enter your email',
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                ),
+                obscureText: true,
+              ),
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  if (errorMessage.isNotEmpty)
+                    Expanded(
+                      child: Text(
+                        errorMessage,
+                        style: const TextStyle(color: Colors.red),
+                      ),
+                    ),
+                  if (errorMessage.isEmpty) const Spacer(),
+                  TextButton(
+                    onPressed: () {
+                      // TODO: Implement forgot password functionality
+                    },
+                    child: const Text('Forgot password?'),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  await signInWithEmail();
                 },
-                child: const Text('Forgot password?'),
-              ),
-            ),
-            const SizedBox(height: 20),
-            SignInButtonBuilder(
-              text: 'Sign in with Email',
-              icon: Icons.email,
-              onPressed: () async {
-                //TODO: Implement sign in with email functionality
-              },
-              backgroundColor: Colors.blueGrey.shade700,
-            ),
-            const SizedBox(height: 20),
-            const Row(
-              children: <Widget>[
-                Expanded(
-                  child: Divider(
-                    height: 20,
+                style: ButtonStyle(
+                  // backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                  // foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                    const EdgeInsets.all(14),
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10),
-                  child: Text(
-                    "OR",
-                  ),
-                ),
-                Expanded(
-                  child: Divider(
-                    height: 20,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                await signInWithGoogle();
-              },
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
-                padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                  const EdgeInsets.fromLTRB(12, 12, 12, 12),
-                ),
-              ),
-              child: Row(
-                mainAxisSize: MainAxisSize.max,
-                children: <Widget>[
-                  Image.asset(
-                    'assets/images/g-logo.png', // Replace with the path to your Google logo
-                    height: 25.0,
-                  ),
-                  const Expanded(
-                    child: Center(
-                      child: Text(
-                        'Sign in with Google',
-                        style: TextStyle(
-                            fontFamily: 'Roboto', fontWeight: FontWeight.w400, fontSize: 18),
+                child: const Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Expanded(
+                      child: Center(
+                        child: Text(
+                          'Sign in',
+                          style: TextStyle(fontWeight: FontWeight.w400, fontSize: 18),
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 10),
-            // SignInButton(
-            //   Buttons.apple,
-            //   onPressed: () async {
-            //     await signInWithApple();
-            //   },
-            // ),
-            ElevatedButton(
-              onPressed: () async {
-                await signInWithApple();
-              },
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
-                padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                  const EdgeInsets.fromLTRB(12, 12, 12, 12),
+                  ],
                 ),
               ),
-              child: const Row(
-                mainAxisSize: MainAxisSize.max,
+              const SizedBox(height: 20),
+              const Row(
                 children: <Widget>[
-                  Icon(
-                    FontAwesomeIcons.apple,
-                    size: 25.0,
+                  Expanded(
+                    child: Divider(
+                      height: 20,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Text(
+                      "OR",
+                    ),
                   ),
                   Expanded(
-                    child: Center(
-                      child: Text(
-                        'Sign in with Apple',
-                        style: TextStyle(fontWeight: FontWeight.w400, fontSize: 18),
-                      ),
+                    child: Divider(
+                      height: 20,
                     ),
                   ),
                 ],
               ),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () async {
-                await signInAnonymously();
-              },
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
-                padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                  const EdgeInsets.fromLTRB(12, 12, 12, 12),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  await signInWithGoogle();
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                  foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                    const EdgeInsets.all(12),
+                  ),
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/images/g-logo.png', // Replace with the path to your Google logo
+                      height: 25.0,
+                    ),
+                    const Expanded(
+                      child: Center(
+                        child: Text(
+                          'Sign in with Google',
+                          style: TextStyle(
+                              fontFamily: 'Roboto', fontWeight: FontWeight.w400, fontSize: 18),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              child: const Row(
-                mainAxisSize: MainAxisSize.max,
-                children: <Widget>[
-                  Icon(
-                    Icons.person, // Use the person icon
-                    size: 25.0,
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () async {
+                  await signInWithApple();
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                  foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                    const EdgeInsets.all(12),
                   ),
-                  Expanded(
-                    child: Center(
-                      child: Text(
-                        'Sign in Anonymously',
-                        style: TextStyle(fontWeight: FontWeight.w400, fontSize: 18),
+                ),
+                child: const Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Icon(
+                      FontAwesomeIcons.apple,
+                      size: 25.0,
+                    ),
+                    Expanded(
+                      child: Center(
+                        child: Text(
+                          'Sign in with Apple',
+                          style: TextStyle(fontWeight: FontWeight.w400, fontSize: 18),
+                        ),
                       ),
                     ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () async {
+                  await signInAnonymously();
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                  foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                    const EdgeInsets.all(12),
                   ),
-                ],
+                ),
+                child: const Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Icon(
+                      Icons.person, // Use the person icon
+                      size: 25.0,
+                    ),
+                    Expanded(
+                      child: Center(
+                        child: Text(
+                          'Sign in Anonymously',
+                          style: TextStyle(fontWeight: FontWeight.w400, fontSize: 18),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(height: 20),
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'By signing in, you agree to our terms and conditions',
-                style: TextStyle(fontSize: 16, color: Colors.grey),
+              const SizedBox(height: 20),
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'By signing in, you agree to our terms and conditions',
+                  style: TextStyle(fontSize: 16, color: Colors.grey),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 
   Future<UserCredential> signInWithGoogle() async {
-    // Trigger the authentication flow
+    // Trigger the Google Sign-In flow
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
-    // Obtain the auth details from the request
-    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+    // If the sign-in was successful, get the credentials
+    if (googleUser != null) {
+      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
 
-    // Create a new credential
-    final credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth?.accessToken,
-      idToken: googleAuth?.idToken,
+      if (googleAuth.accessToken != null || googleAuth.idToken != null) {
+        // Create a new credential
+        final credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken,
+          idToken: googleAuth.idToken,
+        );
+
+        // Once signed in, return the UserCredential
+        return await FirebaseAuth.instance.signInWithCredential(credential);
+      } else {
+        throw FirebaseAuthException(
+          code: 'ERROR_MISSING_GOOGLE_AUTH_TOKEN',
+          message: 'Missing Google Auth Token',
+        );
+      }
+    }
+
+    throw FirebaseAuthException(
+      code: 'ERROR_ABORTED_BY_USER',
+      message: 'Sign in aborted by user',
     );
-
-    // Once signed in, return the UserCredential
-    return await _auth.signInWithCredential(credential);
   }
 
   Future<UserCredential> signInWithApple() async {
@@ -256,6 +302,44 @@ class LoginScreen extends StatelessWidget {
           print("Unknown error.");
       }
       rethrow; // Throw the exception after handling it
+    }
+  }
+
+  Future<void> signInWithEmail() async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      );
+      // If the previous line succeeds, the user is signed in. Clear the error message.
+      setState(() {
+        errorMessage = '';
+      });
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        switch (e.code) {
+          case 'user-not-found':
+            errorMessage = 'No user found for that email.';
+            break;
+          case 'wrong-password':
+            errorMessage = 'Wrong password provided for that user.';
+            break;
+          case 'invalid-email':
+            errorMessage = 'Invalid email provided.';
+            break;
+          case 'user-disabled':
+            errorMessage = 'User disabled.';
+            break;
+          case 'too-many-requests':
+            errorMessage = 'Too many requests. Try again later.';
+            break;
+          case 'operation-not-allowed':
+            errorMessage = 'Operation not allowed.';
+            break;
+          default:
+            errorMessage = 'An unknown error occurred.';
+        }
+      });
     }
   }
 }
